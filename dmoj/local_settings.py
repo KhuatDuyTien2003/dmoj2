d25f61601839071b747ea9cbe0a6aa1aa580257f
KhuatDuyTien2003 <kdt0974758285@gmail.com>
2025-04-07 15:57:34 +0700

    update

--- dmoj/local_settings.py
+++ dmoj/local_settings.py
@@ -31,22 +31,24 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     },
 }
-
+from urllib.parse import urlparse
 # Your database credentials. Only MySQL is supported by DMOJ.
 # Documentation: <https://docs.djangoproject.com/en/3.2/ref/databases/>
+url = urlparse("mysql://root:fBIhINGevgLjZdomggUaaUNbeckveslu@switchback.proxy.rlwy.net:20149/railway")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'fBIhINGevgLjZdomggUaaUNbeckveslu',
-         'HOST': 'mysql.railway.internal',  # đúng theo ảnh
-        'PORT': '3306',  
-        # 'OPTIONS': {
-        #     'charset': 'utf8mb4',
-        #     'sql_mode': 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION',
-        # },
-    },
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
 }
 
 # Sessions.
